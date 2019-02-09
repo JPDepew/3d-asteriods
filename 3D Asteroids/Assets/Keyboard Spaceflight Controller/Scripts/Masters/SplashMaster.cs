882af1e456d668343857fed7e3234b9fe0cb3afe
John Paul Depew <jpdepewjp@gmail.com>
2019-02-09 15:07:05 -0500

    SplashScene working + fonts

---
+++ 3D Asteroids/Assets/Keyboard Spaceflight Controller/Scripts/Masters/SplashMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashMaster : MonoBehaviour
+{
+    public Animator canvasAnim;
+
+    void Start()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(canvasAnim.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
